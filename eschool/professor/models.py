c0ca7a29a9723124ad1aed37fabbee5c45fa4984
judel fintch <judfintch@gmail.com>
2023-03-02 11:15:41 +0200

    feat : make :migration

--- eschool/professor/models.py
+++ eschool/professor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Proffesor(models.Model):
+    fist_name = models.CharField(max_length=60)
+    second_name = models.CharField(max_length=60)
+    qualifiaction = models.CharField(max_length=60)  
+    sexe = models.CharField(max_length=1)
+    birth_day = models.DateField()
